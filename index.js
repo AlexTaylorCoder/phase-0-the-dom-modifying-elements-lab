cbfdbe05fedc64f418bec26ff050d656f12b50a5
alextaylor <alextaylor515@gmail.com>
2022-06-06 17:09:47 -0400

    commit tree 01170134b9c8107867925383224824c072ce186d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")
+main[0].remove()
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+
+newHeader.textContent = "YOUR-NAME is the champion"
